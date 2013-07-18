d0332ae07ea5b933d66645ea9bcddb0769443d29
Apostolis <ap.giannakidis@gmail.com>
2013-07-18 04:05:36 +0300

    Added properties support and flexible kvclient setup.

--- gora-oracle/src/main/java/org/apache/gora/oracle/store/OracleStore.java
+++ gora-oracle/src/main/java/org/apache/gora/oracle/store/OracleStore.java
@@ -16,41 +16,179 @@
  */
 package org.apache.gora.oracle.store;
 
+import oracle.kv.*;
 import org.apache.gora.persistency.impl.PersistentBase;
 import org.apache.gora.query.PartitionQuery;
 import org.apache.gora.query.Query;
 import org.apache.gora.query.Result;
+import org.apache.gora.store.DataStoreFactory;
 import org.apache.gora.store.impl.DataStoreBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
-
-import oracle.kv.KVStore;
-import oracle.kv.KVStoreFactory;
-import oracle.kv.KVStoreConfig;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Apostolos Giannakidis
  */
 public class OracleStore<K,T extends PersistentBase> extends DataStoreBase<K,T> {
 
+    private static final Logger LOG = LoggerFactory.getLogger(OracleStore.class);
+
     private static final String DEFAULT_MAPPING_FILE = "gora-oracle-mapping.xml";
-    private static String storeName = "kvstore";
-    private static String hostName = "localhost";
-    private static String hostPort = "5000";
+    private static final String DURABILITY_SYNCPOLICY = "gora.oraclenosql.durability.syncpolicy";
+    private static final String DURABILITY_REPLICAACKPOLICY = "gora.oraclenosql.durability.replicaackpolicy";
+    private static final String CONSISTENCY = "gora.oraclenosql.consistency";
+    private static final String TIME_UNIT = "gora.oraclenosql.time.unit";
+    private static final String REQUEST_TIMEOUT = "gora.oraclenosql.request.timeout";
+    private static final String READ_TIMEOUT = "gora.oraclenosql.read.timeout";
+    private static final String LOB_TIMEOUT = "gora.oraclenosql.lob.timeout";
+    private static final String OPEN_TIMEOUT = "gora.oraclenosql.open.timeout";
+    private static final String STORE_NAME = "gora.oraclenosql.storename";
+    private static final String HOST_NAME = "gora.oraclenosql.hostname";
+    private static final String HOST_PORT = "gora.oraclenosql.hostport";
+
+    private static final String DEFAULT_STORE_NAME = "kvstore";
+    private static final String DEFAULT_HOST_NAME = "localhost";
+    private static final String DEFAULT_HOST_PORT = "5000";
 
     private volatile OracleMapping mapping;
 
+    private static String storeName;
+    private static String hostName;
+    private static String hostPort;
+    private static String mappingFile;
+
+    private static int lobTimeout;
+    private static int readTimeout;
+    private static int openTimeout;
+    private static int requestTimeout;
+
+    private static Durability.ReplicaAckPolicy durabilityReplicaAckPolicy;
+    private static Durability.SyncPolicy durabilitySyncPolicy;
+    private static Consistency consistency;
+    private static TimeUnit timeUnit;
+
     private KVStore kvstore;
+    private KVStoreConfig conf;
 
     @Override
     public void initialize(Class<K> keyClass, Class<T> persistentClass, Properties properties) {
         super.initialize(keyClass, persistentClass, properties);
 
-        kvstore = KVStoreFactory.getStore
-                (new KVStoreConfig(storeName, hostName + ":" + hostPort));
+        readProperties(properties);
+
+        setupClient();
+
+        try {
+            mapping = readMapping( mappingFile );
+        }
+        catch ( IOException e ) {
+            LOG.error( e.getMessage() );
+            LOG.error( e.getStackTrace().toString() );
+        }
     }
+
+    private void setupClient(){
+
+        conf = new KVStoreConfig(storeName, hostName + ":" + hostPort);
+
+        conf.setRequestTimeout(requestTimeout, timeUnit);
+        conf.setLOBTimeout(lobTimeout, timeUnit);
+        conf.setSocketReadTimeout(readTimeout, timeUnit);
+        conf.setSocketOpenTimeout(openTimeout, timeUnit);
+        conf.setConsistency(consistency);
+        Durability newDurability = new Durability(durabilitySyncPolicy,    // Master sync
+                                                  durabilitySyncPolicy, // Replica sync
+                                                  durabilityReplicaAckPolicy);
+        conf.setDurability(newDurability);
+
+        kvstore = KVStoreFactory.getStore(conf);
+    }
+
+    private void readProperties(Properties properties) {
+
+        mappingFile = DataStoreFactory.getMappingFile(properties, this, DEFAULT_MAPPING_FILE);
+        storeName = DataStoreFactory.findProperty( properties, this, STORE_NAME, DEFAULT_STORE_NAME );
+        hostName = DataStoreFactory.findProperty( properties, this, HOST_NAME, DEFAULT_HOST_NAME );
+        hostPort = DataStoreFactory.findProperty( properties, this, HOST_PORT, DEFAULT_HOST_PORT);
+
+        try{
+            requestTimeout = Integer.parseInt(DataStoreFactory.findProperty( properties, this, REQUEST_TIMEOUT, String.valueOf(KVStoreConfig.DEFAULT_REQUEST_TIMEOUT)));
+        }
+        catch ( NumberFormatException nfe ) {
+            requestTimeout = KVStoreConfig.DEFAULT_REQUEST_TIMEOUT;
+            LOG.warn( "Invalid requestTimeout value. Using default " + String.valueOf(KVStoreConfig.DEFAULT_REQUEST_TIMEOUT) );
+        }
+
+        try{
+            readTimeout = Integer.parseInt(DataStoreFactory.findProperty( properties, this, READ_TIMEOUT, String.valueOf(KVStoreConfig.DEFAULT_READ_TIMEOUT)));
+        }
+        catch ( NumberFormatException nfe ) {
+            readTimeout = KVStoreConfig.DEFAULT_READ_TIMEOUT;
+            LOG.warn( "Invalid readTimeout value. Using default " + String.valueOf(KVStoreConfig.DEFAULT_READ_TIMEOUT) );
+        }
+
+        try{
+            lobTimeout = Integer.parseInt(DataStoreFactory.findProperty( properties, this, LOB_TIMEOUT, String.valueOf(KVStoreConfig.DEFAULT_LOB_TIMEOUT)));
+        }
+        catch ( NumberFormatException nfe ) {
+            lobTimeout = KVStoreConfig.DEFAULT_LOB_TIMEOUT;
+            LOG.warn( "Invalid lobTimeout value. Using default " + String.valueOf(KVStoreConfig.DEFAULT_LOB_TIMEOUT) );
+        }
+
+        try{
+            openTimeout = Integer.parseInt(DataStoreFactory.findProperty( properties, this, OPEN_TIMEOUT, String.valueOf(KVStoreConfig.DEFAULT_OPEN_TIMEOUT)));
+        }
+        catch ( NumberFormatException nfe ) {
+            openTimeout = KVStoreConfig.DEFAULT_OPEN_TIMEOUT;
+            LOG.warn( "Invalid openTimeout value. Using default " + String.valueOf(KVStoreConfig.DEFAULT_OPEN_TIMEOUT) );
+        }
+
+        durabilityReplicaAckPolicy = Durability.ReplicaAckPolicy.valueOf(DataStoreFactory.findProperty( properties, this, DURABILITY_REPLICAACKPOLICY, Durability.ReplicaAckPolicy.SIMPLE_MAJORITY.name() ));
+        durabilitySyncPolicy = Durability.SyncPolicy.valueOf(DataStoreFactory.findProperty( properties, this, DURABILITY_SYNCPOLICY, Durability.SyncPolicy.WRITE_NO_SYNC.name() ));
+
+        String tmpConsistency = DataStoreFactory.findProperty( properties, this, CONSISTENCY, Consistency.NONE_REQUIRED.getName() );
+
+        if (tmpConsistency.equals("NONE_REQUIRED"))
+            consistency = Consistency.NONE_REQUIRED;
+        else if (tmpConsistency.equals("ABSOLUTE"))
+            consistency = Consistency.ABSOLUTE;
+        else{
+            consistency = Consistency.NONE_REQUIRED;
+            LOG.debug("Consistency was set to default.");
+        }
+
+        String tmpTimeUnit = DataStoreFactory.findProperty( properties, this, TIME_UNIT, null );
+
+        if (tmpTimeUnit.equals("DAYS"))
+            timeUnit = TimeUnit.DAYS;
+        else if (tmpTimeUnit.equals("HOURS"))
+            timeUnit = TimeUnit.HOURS;
+        else if (tmpTimeUnit.equals("MICROSECONDS"))
+            timeUnit = TimeUnit.MICROSECONDS;
+        else if (tmpTimeUnit.equals("MILLISECONDS"))
+            timeUnit = TimeUnit.MILLISECONDS;
+        else if (tmpTimeUnit.equals("MINUTES"))
+            timeUnit = TimeUnit.MINUTES;
+        else if (tmpTimeUnit.equals("NANOSECONDS"))
+            timeUnit = TimeUnit.NANOSECONDS;
+        else if (tmpTimeUnit.equals("SECONDS"))
+            timeUnit = TimeUnit.SECONDS;
+        else{
+            LOG.error("timeUnit was invalid.");
+            throw new IllegalStateException();
+        }
+
+    }
+
+    private OracleMapping readMapping(String mappingFile) throws IOException {
+        return null;
+    }
+
 
     @Override
     public String getSchemaName() {
@@ -83,6 +221,7 @@ public class OracleStore<K,T extends PersistentBase> extends DataStoreBase<K,T> 
     @Override
     public void put(K key, T obj) {
         //TODO
+
     }
 
     @Override
